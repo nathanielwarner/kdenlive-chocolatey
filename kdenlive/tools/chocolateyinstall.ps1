38e398ffb86a37e31bf66ff983d313935410a4db
Nathaniel Warner <nathanielwarner77@gmail.com>
2018-06-25 18:21:11 -0500

    Add start menu shortcut

--- kdenlive/tools/chocolateyinstall.ps1
+++ kdenlive/tools/chocolateyinstall.ps1
@@ -26,7 +26,16 @@ $softwareName  = 'kdenlive*'
 $checksum64    = 'D1BC33D1C32F9E2343D9A91BCD6DEB864C5C598A08C88DC2E8F7AA46CC45D35F'
 $checksumType64= 'sha256'
 
+$fileName = "Kdenlive-18.04.1\kdenlive.exe"
+$linkName = "kdenlive.lnk"
+$destdir = $(Split-Path -parent $MyInvocation.MyCommand.Definition)
+
 Install-ChocolateyZipPackage -PackageName $packageName -Url64Bit $url64 -UnzipLocation $toolsDir -Checksum64 $checksum64 -ChecksumType64 $checksumType64
+
+$programs = [environment]::GetFolderPath([environment+specialfolder]::Programs)
+$shortcutFilePath = Join-Path $programs $linkName
+$targetPath = Join-Path $destdir $fileName
+Install-ChocolateyShortcut -shortcutFilePath $shortcutFilePath -targetPath $targetPath
     
 ## add specific folders to the path - any executables found in the chocolatey package 
 ## folder will already be on the path. This is used in addition to that or for cases 
