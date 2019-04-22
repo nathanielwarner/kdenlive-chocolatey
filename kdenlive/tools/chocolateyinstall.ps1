b4240c9acf7df157d15f13edb951d2b86d6b4c55
Nathaniel Warner <nathanielwarner77@gmail.com>
2019-04-22 10:08:16 -0500

    Update to 19.04.0

--- kdenlive/tools/chocolateyinstall.ps1
+++ kdenlive/tools/chocolateyinstall.ps1
@@ -2,9 +2,9 @@
     PackageName = 'kdenlive'
     FileType = 'EXE'
     SilentArgs = '/S'
-    Url = 'https://files.kde.org/kdenlive/release/kdenlive-18.12.1.exe'
+    Url = 'https://files.kde.org/kdenlive/release/kdenlive-19.04.0-1.exe'
     ChecksumType = 'sha256'
-    Checksum = 'F37F148922244999080FACD5EFE796CBE785F66C78008335D7F356640EDC2A5A'
+    Checksum = '721E711E586ACC72CFFF5F9A0C7BE0BEA777D0122EC4F29539A2EDD9DBF6FA13'
 }
 
 Install-ChocolateyPackage @InstallArgs
