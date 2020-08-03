8938048e87ffa662e83606a98783245535f18d2e
ares999-cloud <69145461+ares999-cloud@users.noreply.github.com>
2020-08-03 15:25:17 +0530

    Update chocolateyinstall.ps1

--- kdenlive/tools/chocolateyinstall.ps1
+++ kdenlive/tools/chocolateyinstall.ps1
@@ -2,7 +2,7 @@
     PackageName = 'kdenlive'
     FileType = 'EXE'
     SilentArgs = '/S'
-    Url = 'https://files.kde.org/kdenlive/release/kdenlive-20.04.1.exe'
+    Url = 'https://iso.mirrors.ustc.edu.cn/kde-application/kdenlive/release/kdenlive-20.04.1.exe'
     ChecksumType = 'sha256'
     Checksum = '62DB34513D7C58A5A7F9CBD88C0FCA985D10C0FFDDEB8BED6A451CC968B2C81C'
 }
