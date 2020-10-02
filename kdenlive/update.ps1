f59e3b481bb68f5da790ec5bcc7cbc2f07ac3ae8
TheCakeIsNaOH <TheCakeIsNaOH@gmail.com>
2020-10-02 17:56:48 -0500

    add AU update script

---
+++ kdenlive/update.ps1
@@ -0,0 +1,26 @@
+﻿Import-Module AU
+
+$releases = 'https://kdenlive.org/en/download/'
+
+function global:au_SearchReplace {
+    @{
+        "tools\VERIFICATION.txt" = @{
+			"(?i)(\s+x32:).*" = "`${1} $($Latest.URL32)"
+		}
+	}
+}
+
+function global:au_BeforeUpdate() {
+	Get-RemoteFiles -Purge -NoSuffix 
+}
+
+
+function global:au_GetLatest {
+    $download_page = Invoke-WebRequest -Uri $releases -UseBasicParsing
+    $url        = $download_page.links | ? href -match '\d.exe$'| % href | select -First 1
+	$version    = (($url -split '/' | select -Last 1) -split '-' | select -Last 1).trim('.exe')
+	
+	return @{ Version = $version; URL32 = $url;}
+}
+
+Update-Package -ChecksumFor none
